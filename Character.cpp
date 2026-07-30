c52e15cce38e12145ae332b48e3187d3a61b22cf
Ashanti Campbell <acampbell20@cub.uca.edu>
2026-07-30 13:02:06 -0500

    Implement Character class with health management

--- Character.cpp
+++ Character.cpp
@@ -1,1 +1,30 @@
+#include "Character.h"
 
+Character::Character(string n, int h)
+{
+    name = n;
+    health = h;
+}
+
+void Character::takeDamage(int damage)
+{
+    health -= damage;
+
+    if (health < 0)
+        health = 0;
+}
+
+bool Character::isAlive() const
+{
+    return health > 0;
+}
+
+string Character::getName() const
+{
+    return name;
+}
+
+int Character::getHealth() const
+{
+    return health;
+}
