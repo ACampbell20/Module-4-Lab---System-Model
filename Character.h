1187001ac6579ba516f745e9afe6fc1dbeb5f926
Ashanti Campbell <acampbell20@cub.uca.edu>
2026-07-30 13:00:48 -0500

    Define Character class with basic attributes and methods

--- Character.h
+++ Character.h
@@ -1,1 +1,27 @@
+#ifndef CHARACTER_H
+#define CHARACTER_H
 
+#include <string>
+using namespace std;
+
+class Character
+{
+protected:
+    string name;
+    int health;
+
+public:
+    Character(string n, int h);
+
+    virtual void attack() = 0;
+
+    void takeDamage(int damage);
+    bool isAlive() const;
+
+    string getName() const;
+    int getHealth() const;
+
+    virtual ~Character() {}
+};
+
+#endif
