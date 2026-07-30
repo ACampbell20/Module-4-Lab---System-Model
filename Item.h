15bd3341e25d8c50bff7331a8570ec982e448c00
Ashanti Campbell <acampbell20@cub.uca.edu>
2026-07-30 13:03:33 -0500

    Define Item class with protected itemName and methods

--- Item.h
+++ Item.h
@@ -1,1 +1,21 @@
+#ifndef ITEM_H
+#define ITEM_H
 
+#include <string>
+using namespace std;
+
+class Item{
+protected:
+    string itemName;
+
+public:
+    Item(string name);
+
+    virtual void use() = 0;
+
+    string getItemName() const;
+
+    virtual ~Item() {}
+};
+
+#endif
