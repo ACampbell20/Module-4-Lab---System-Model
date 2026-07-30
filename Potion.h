1f2ccf03f6561c32afb14da0ac111419e2a0deb2
Ashanti Campbell <acampbell20@cub.uca.edu>
2026-07-30 13:04:21 -0500

    Add Potion class with healing functionality

--- Potion.h
+++ Potion.h
@@ -1,1 +1,18 @@
+#ifndef POTION_H
+#define POTION_H
 
+#include "Item.h"
+
+class Potion : public Item{
+private:
+    int healAmount;
+
+public:
+    Potion();
+
+    void use() override;
+
+    int getHealAmount() const;
+};
+
+#endif
