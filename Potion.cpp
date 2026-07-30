28c26ca821687fb3255bff67517750508fae72ad
Ashanti Campbell <acampbell20@cub.uca.edu>
2026-07-30 13:04:42 -0500

    Implement Potion class with healing functionality

--- Potion.cpp
+++ Potion.cpp
@@ -1,1 +1,17 @@
+#include <iostream>
+#include "Potion.h"
 
+using namespace std;
+
+Potion::Potion() : Item("Health Potion"){
+    healAmount = 25;
+}
+
+void Potion::use(){
+    cout << "You used a Health Potion and restored "
+         << healAmount << " health!" << endl;
+}
+
+int Potion::getHealAmount() const{
+    return healAmount;
+}
