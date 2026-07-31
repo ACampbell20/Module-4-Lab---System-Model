ffdc322878fd8a96c08a134627e7ec1c967705ff
Ashanti Campbell <acampbell20@cub.uca.edu>
2026-07-31 10:20:25 -0500

    Define Enemy class with attack and damage methods

--- Enemy.h
+++ Enemy.h
@@ -1,1 +1,19 @@
+#ifndef ENEMY_H
+#define ENEMY_H
 
+#include "Character.h"
+
+class Enemy : public Character
+{
+private:
+    int damage;
+
+public:
+    Enemy(string n, int h, int d);
+
+    void attack() override;
+
+    int getDamage() const;
+};
+
+#endif
