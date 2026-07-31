79772cfddb17b32588c52ca9ed5da3c91c96b630
Ashanti Campbell <acampbell20@cub.uca.edu>
2026-07-31 10:23:12 -0500

    Add Game class with player and enemy members

--- Game.h
+++ Game.h
@@ -1,1 +1,18 @@
+#ifndef GAME_H
+#define GAME_H
 
+#include "Player.h"
+#include "Enemy.h"
+
+class Game{
+private:
+    Player player;
+    Enemy enemy;
+
+public:
+    Game();
+
+    void startGame();
+};
+
+#endif
