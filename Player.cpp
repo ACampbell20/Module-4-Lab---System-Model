1a90b55f19ecb0cfc4d2bcad25f7d5929960d4ea
Ashanti Campbell <acampbell20@cub.uca.edu>
2026-07-31 10:19:40 -0500

    Update Player.cpp

--- Player.cpp
+++ Player.cpp
@@ -1,1 +1,25 @@
+#include <iostream>
+#include "Player.h"
 
+using namespace std;
+
+Player::Player(string n, int h)
+    : Character(n, h){
+}
+
+void Player::attack(){
+    cout << name << " swings a sword for 20 damage!" << endl;
+}
+
+void Player::usePotion(){
+    potion.use();
+
+    health += potion.getHealAmount();
+
+    cout << name << "'s health is now "
+         << health << "." << endl;
+}
+
+Potion Player::getPotion() const{
+    return potion;
+}
