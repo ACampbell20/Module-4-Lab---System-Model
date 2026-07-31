21fa920af624bb474f5b766ca9c4f66c32c03709
Ashanti Campbell <acampbell20@cub.uca.edu>
2026-07-31 10:21:05 -0500

    Implement Enemy class with attack and damage methods

--- Enemy.cpp
+++ Enemy.cpp
@@ -1,1 +1,18 @@
+#include <iostream>
+#include "Enemy.h"
 
+using namespace std;
+
+Enemy::Enemy(string n, int h, int d)
+    : Character(n, h){
+    damage = d;
+}
+
+void Enemy::attack(){
+    cout << name << " attacks for "
+         << damage << " damage!" << endl;
+}
+
+int Enemy::getDamage() const{
+    return damage;
+}
