d4bba9e0aacdc899b70153fd3b9ffb1bcf9c1098
Ashanti Campbell <acampbell20@cub.uca.edu>
2026-07-31 10:19:08 -0500

    Add Player class with attack and potion methods

--- Player.h
+++ Player.h
@@ -1,1 +1,21 @@
+#ifndef PLAYER_H
+#define PLAYER_H
 
+#include "Character.h"
+#include "Potion.h"
+
+class Player : public Character{
+private:
+    Potion potion;
+
+public:
+    Player(string n, int h);
+
+    void attack() override;
+
+    void usePotion();
+
+    Potion getPotion() const;
+};
+
+#endif
