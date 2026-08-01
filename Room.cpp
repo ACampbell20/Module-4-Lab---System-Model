2dcbda39bff6cf6fd6c8622515092337b7d53298
Ashanti Campbell <acampbell20@cub.uca.edu>
2026-08-01 16:17:18 -0500

    Implement Room class with basic functionality

--- Room.cpp
+++ Room.cpp
@@ -1,1 +1,32 @@
+#include "Room.h"
 
+Room::Room(string desc)
+{
+    description = desc;
+    enemy = nullptr;
+}
+
+void Room::setEnemy(Enemy* e)
+{
+    enemy = e;
+}
+
+void Room::addItem(Item* item)
+{
+    items.push_back(item);
+}
+
+string Room::getDescription() const
+{
+    return description;
+}
+
+Enemy* Room::getEnemy() const
+{
+    return enemy;
+}
+
+vector<Item*> Room::getItems() const
+{
+    return items;
+}
