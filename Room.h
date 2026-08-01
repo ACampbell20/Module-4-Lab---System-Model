340af4f223fa21b2f44d64cce7670a0c6f48bbf0
Ashanti Campbell <acampbell20@cub.uca.edu>
2026-08-01 16:16:49 -0500

    Add Room class with enemy and item management
    Define Room class with properties and methods for managing enemies and items.

--- Room.h
+++ Room.h
@@ -1,1 +1,28 @@
+#ifndef ROOM_H
+#define ROOM_H
 
+#include <string>
+#include <vector>
+#include "Enemy.h"
+#include "Item.h"
+using namespace std;
+
+class Room
+{
+private:
+    string description;
+    Enemy* enemy;
+    vector<Item*> items;
+
+public:
+    Room(string desc = "");
+
+    void setEnemy(Enemy* e);
+    void addItem(Item* item);
+
+    string getDescription() const;
+    Enemy* getEnemy() const;
+    vector<Item*> getItems() const;
+};
+
+#endif
