ba27daddee5dbe3b72ee1f64affc2d4e228e815d
Ashanti Campbell <acampbell20@cub.uca.edu>
2026-08-01 16:16:37 -0500

    Implement Weapon class with basic functionality

--- Weapon.cpp
+++ Weapon.cpp
@@ -1,1 +1,20 @@
+#include <iostream>
+#include "Weapon.h"
 
+using namespace std;
+
+Weapon::Weapon(string name, int power)
+    : Item(name)
+{
+    attackPower = power;
+}
+
+void Weapon::use()
+{
+    cout << itemName << " used." << endl;
+}
+
+int Weapon::getAttackPower() const
+{
+    return attackPower;
+}
