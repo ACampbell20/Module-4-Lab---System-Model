028a917eb58efd3a9560647961124d81562774f8
Ashanti Campbell <acampbell20@cub.uca.edu>
2026-08-01 16:16:17 -0500

    Add Weapon class with attack power functionality

--- Weapon.h
+++ Weapon.h
@@ -1,1 +1,21 @@
+#ifndef WEAPON_H
+#define WEAPON_H
 
+#include "Item.h"
+#include <string>
+using namespace std;
+
+class Weapon : public Item
+{
+private:
+    int attackPower;
+
+public:
+    Weapon(string name, int power);
+
+    void use() override;
+
+    int getAttackPower() const;
+};
+
+#endif
